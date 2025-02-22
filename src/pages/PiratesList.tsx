4b51d7a8c5c7a075230a731d9b5b7c8a037e99cf
Antoine Thompson <antoine.thompson@outlook.fr>
2025-02-22 22:37:03 +0100

    feat : ajust css piece

--- src/pages/PiratesList.tsx
+++ src/pages/PiratesList.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 const PiratesList = () => {
 
-
   const [joueurs, setJoueurs] = useState<{
     id: number,
     nom: string,
@@ -10,59 +9,56 @@ const PiratesList = () => {
   }[]>([]);
 
   useEffect(() => {
-    console.log('Fetching data from the API...')
     getJoueursSorted();
   }, [])
 
-    const getJoueursSorted = () => {
-      fetch('/api/joueurs/sorted')
+  const getJoueursSorted = () => {
+    fetch('/api/joueurs/sorted')
       .then(response => response.json())
       .then(data => setJoueurs(data))
       .catch((error) => console.error('Error:', error));
-    }
-    const modifierNbPieces = (e:any,id: number, value: number) => {
-      e.preventDefault();
-      console.log('Ajouter une pièce pour le joueur avec l\'id', id);
-      fetch('/api/joueurs/'+id,{
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          value
-        })
+  }
+  
+  const modifierNbPieces = (e: any, id: number, value: number) => {
+    e.preventDefault();
+    console.log('Ajouter une pièce pour le joueur avec l\'id', id);
+    fetch('/api/joueurs/' + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        value
       })
+    })
       .then(response => response.json())
       .then(data => setJoueurs(data))
       .catch((error) => console.error('Error:', error));
-    }
-    return (
+  }
+  return (
     <>
-    <h1>🏴‍☠️ Liste des Pirates</h1>
-    <div >
-      {joueurs.map(joueur => (
-        <div key={joueur.id} className="grid grid-cols-5 self-center">
-          <div className="col-span-3"> 
-            {joueur.nom}
-          </div>
-          <div>
-{joueur.piecesOr} 💰
+      <h1>🏴‍☠️ Liste des Pirates</h1>
+      <div >
+        {joueurs.map(joueur => (
+          <div key={joueur.id} className="grid grid-cols-5 self-center">
+            <div className="col-span-3 flex gap-1">
+              <div>
+                {joueur.piecesOr}💰
+              </div>
+              {joueur.nom}
             </div>
-          <div>
+            <div className="flex col-span-2 gap-4 mx-auto">
+              <button onClick={(e) => modifierNbPieces(e, joueur.id, 1)}>✚💰</button>
+              <button onClick={(e) => modifierNbPieces(e, joueur.id, -1)}>−💰</button>
+              <button onClick={(e) => modifierNbPieces(e, joueur.id, -4)}>✉️</button>
+            </div>
 
-          <button onClick={(e)=>modifierNbPieces(e,joueur.id, 1)}>✚💰</button>
-          <button onClick={(e)=>modifierNbPieces(e,joueur.id, -1)}>−💰</button>
-
-          <button onClick={(e)=>modifierNbPieces(e,joueur.id, -4)}>✉️</button>
-           </div> 
-        
-        </div>
-      ))}
-    </div>
-    <button onClick={getJoueursSorted}>Actualiser classement</button>
+          </div>
+        ))}
+      </div>
+      <button onClick={getJoueursSorted}>Actualiser classement</button>
     </>
-    );
-  };
-  
-  export default PiratesList;
-  
+  );
+};
+
+export default PiratesList;
