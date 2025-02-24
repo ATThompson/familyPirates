0c1466d00224ca76d5fc8ccec5737089a5d463c9
ATThompson <antoine.thompson@outlook.fr>
2025-02-24 12:00:58 +0100

    feat: regle blind test

--- src/pages/rules/BlindTest.tsx
+++ src/pages/rules/BlindTest.tsx
@@ -1,5 +1,57 @@
 const BlindTest = () => {
-  return <h1>Blind Test</h1>
+  return (
+    <>
+      <h2>📜 Blind Test des Sirènes </h2>
+      <div>
+        <div>
+
+          Ahoy, moussaillons ! Préparez-vous pour un défi musical où chaque pirate joue pour lui-même. Il y aura 16 écoutes au total, et seul le pirate le plus rusé saura prendre tout l'or du trésor !
+        </div>
+        <h3>
+          🎶 Partie Musique des Flibustiers
+        </h3>
+        <ul>
+          <li>
+
+            1. Huit morceaux de musique seront joués, avec un total de 16 écoutes à deviner pendant la partie.
+          </li>
+          <li>
+
+            2. 1 pièce d’or sera attribuée au pirate qui devine le chanteur de la chanson. 🎤
+          </li>
+          <li>
+
+            3. 1 pièce d’or supplémentaire pour le pirate qui devine le titre exact de la musique. 🎶
+          </li>
+          <li>
+
+            4. Attention, l’esprit des pirates flotte dans chaque mélodie – seule une oreille fine saura saisir les indices cachés ! 🌊
+          </li>
+        </ul>
+        {' '}
+        🎶
+        <h3>
+          🎬 Partie Bande Son de Film des Corsaires 🎬
+        </h3>
+        <ul>
+          <li>
+            1. Huit bandes sonores de films célèbres seront jouées, totalisant encore 16 écoutes pour nos pirates affûtés. 🍿
+          </li>
+          <li>
+            2. 1 pièce d’or sera attribuée au pirate qui devine correctement le titre du film. 🎥
+          </li>
+          <li>
+            3. 1 pièce d’or supplémentaire pour celui qui peut nommer l’acteur principal ou le réalisateur du film. 🎬
+          </li>
+          <li>
+            4. Les vagues cinématographiques ne vous feront pas de cadeau – chaque pirate devra prêter une oreille de lynx ! 🏝️
+          </li>
+        </ul>
+        Cout de la participation au jeu une pièce d'or.
+
+      </div>
+    </>
+  )
 }
 
 export default BlindTest
