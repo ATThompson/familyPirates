e307f0d971d45f15d1105ef82f54c7b778262195
ATThompson <antoine.thompson@outlook.fr>
2025-02-24 13:01:18 +0100

    feat : ajout règles enigmes

--- src/pages/rules/BlindTest.tsx
+++ src/pages/rules/BlindTest.tsx
@@ -1,49 +1,45 @@
 const BlindTest = () => {
   return (
-    <>
-      <div className="flex flex-col gap-y-4">
-
-        <h2>📜 Blind Test 📜</h2>
-
-        <div>
-          Ahoy, moussaillons ! Préparez-vous pour un défi musical où chaque pirate joue pour lui-même. Il y aura 16 écoutes au total, et seul le pirate le plus rusé saura prendre tout l'or du trésor !
-        </div>
-        <h3>
-          🎶Musique des Flibustiers🎶
-        </h3>
-        <ul className="list-disc list-inside px-4 flex flex-col gap-y-1">
-          <li>
-            8 morceaux de musique seront joués.
-          </li>
-          <ul className="list-disc list-inside pl-4">
-            <li>
-              1 pièce d’or sera attribuée au pirate qui devine le chanteur de la chanson. 🎤
-            </li>
-            <li>
-              1 pièce d’or supplémentaire pour le pirate qui devine le titre exact de la musique. 🎶
-            </li>
-          </ul>
-        </ul>
-
-        <h3>
-          🎬 Film des Corsaires🎬
-        </h3>
-        <ul className="list-disc list-inside px-4 flex flex-col gap-y-1">
-          <li>
-            8 bandes sonores de films célèbres seront jouées.
-          </li>
-          <ul className="list-disc list-inside pl-4">
-            <li>
-              1 pièce d’or sera attribuée au pirate qui devine correctement le titre du film. 🎥
-            </li>
-            <li>
-              1 pièce d’or supplémentaire pour celui qui peut nommer l’acteur principal ou le réalisateur du film. 🎬
-            </li>
-          </ul>
-        </ul>
-        Cout de la participation au jeu : 1💰
+    <div className="flex flex-col gap-y-4">
+      <h2>📜 Règles 📜</h2>
+      <div>
+        Ahoy, moussaillons ! Préparez-vous pour un défi musical où chaque pirate joue pour lui-même. Il y aura 16 écoutes au total, et seul le pirate le plus rusé saura prendre tout l'or du trésor !
       </div>
-    </>
+      <h3>
+        🎶Musique des Flibustiers🎶
+      </h3>
+      <ul className="list-disc list-inside px-4 flex flex-col gap-y-1">
+        <li>
+          8 morceaux de musique seront joués.
+        </li>
+        <ul className="list-disc list-inside pl-4">
+          <li>
+            1 pièce d’or sera attribuée au pirate qui devine le chanteur de la chanson. 🎤
+          </li>
+          <li>
+            1 pièce d’or supplémentaire pour le pirate qui devine le titre exact de la musique. 🎶
+          </li>
+        </ul>
+      </ul>
+
+      <h3>
+        🎬 Film des Corsaires🎬
+      </h3>
+      <ul className="list-disc list-inside px-4 flex flex-col gap-y-1">
+        <li>
+          8 bandes sonores de films célèbres seront jouées.
+        </li>
+        <ul className="list-disc list-inside pl-4">
+          <li>
+            1 pièce d’or sera attribuée au pirate qui devine correctement le titre du film. 🎥
+          </li>
+          <li>
+            1 pièce d’or supplémentaire pour celui qui peut nommer l’acteur principal ou le réalisateur du film. 🎬
+          </li>
+        </ul>
+      </ul>
+      Cout de la participation au jeu : 1💰.
+    </div>
   )
 }
 
--- src/pages/rules/Enigmes.tsx
+++ src/pages/rules/Enigmes.tsx
@@ -1,5 +1,33 @@
 const Enigmes = () => {
-  return <h1> Enigmes </h1>
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h2>📜 Règles 📜</h2>
+      <div>
+        Ahoy, pirates ! Préparez-vous à défier vos esprits avec des énigmes.
+      </div>
+      <h3>
+        🏺 Le Pot des Énigmes 🏺
+      </h3>
+      <ul className="list-disc list-inside px-4 flex flex-col gap-y-1">
+        <li>
+          Un pot est rempli d'énigmes, prêtes à être dévoilées à ceux assez audacieux pour y plonger.
+        </li>
+        <li>
+          Pour prendre une énigme, le pirate doit déposer une pièce d’or dans le pot. 💰
+        </li>
+        <ul className="list-disc list-inside pl-4">
+          <li>
+            Une seule réponse est autorisée. Si le pirate donne la bonne réponse, il remporte une pièce d’or en récompense.
+          </li>
+          <li>
+            Si le pirate donne la mauvaise réponse, il perd une pièce d’or. 🏴‍☠️
+          </li>
+        </ul>
+
+      </ul>
+      Cout de la participation 1💰par enigme.
+    </div>
+  )
 }
 
 export default Enigmes
