23b497acca5a8ee6c95fa845fc5c0a01eb7a6189
ATThompson <antoine.thompson@outlook.fr>
2025-02-25 11:52:50 +0100

    fix : css for ios

--- src/components/Parchment.tsx
+++ src/components/Parchment.tsx
@@ -40,12 +40,12 @@ const Parchment = ({ classNames, children }: {
     <div className={`wavy-container ${classNames}`}>
       <svg className="hidden">
         <filter id="wavy2">
-          <feTurbulence x="0" y="0" baseFrequency="0.0005" numOctaves="2" seed="1" result="turbulence" />
+          <feTurbulence x="0" y="0" baseFrequency="0.01 0.015" numOctaves="6" seed="1" result="turbulence" />
           <feDisplacementMap in="SourceGraphic" in2="turbulence" scale="15" />
         </filter>
       </svg>
 
-      <div className="wavy-parchment" ref={parchmentRef} style={{ height, transition: 'height 0.3s ease-in-out' }}>
+      <div className="wavy-parchment" ref={parchmentRef}>
         <div className="wavy-bg "></div>
         <div ref={contentRef} className={`content `}>{children}</div>
       </div>
