1219f54cd4e5ce4c51657e95e7b47c2f19543c0b
Antoine Thompson <antoine.thompson@outlook.fr>
2025-02-27 18:38:58 +0100

    feat : update pieces d'or

--- server/src/joueurs.ts
+++ server/src/joueurs.ts
@@ -3,69 +3,69 @@ export const joueurs: {
   nom: string
   piecesOr: number
 }[] = [
-  {
-    id: 2,
-    nom: 'Quartier Maître Chloé',
-    piecesOr: 5,
-  },
-  {
-    id: 3,
-    nom: 'Seconde Emma',
-    piecesOr: 5,
-  },
-  {
-    id: 4,
-    nom: 'Maître d\'équipage Fredo',
-    piecesOr: 5,
-  },
-  {
-    id: 5,
-    nom: 'Cuistot Sylvie',
-    piecesOr: 5,
-  },
-  {
-    id: 6,
-    nom: 'Maître canonnier Julien',
-    piecesOr: 5,
-  },
-  {
-    id: 7,
-    nom: 'Marie la Guerta',
-    piecesOr: 5,
-  },
-  {
-    id: 8,
-    nom: 'Navigateur Bruno',
-    piecesOr: 5,
-  },
-  {
-    id: 9,
-    nom: 'Cargueuse Cathy',
-    piecesOr: 5,
-  },
-  {
-    id: 10,
-    nom: 'Contrebandier Gauthier',
-    piecesOr: 5,
-  },
-  {
-    id: 11,
-    nom: 'Canonnière Margot',
-    piecesOr: 5,
-  },
-  {
-    id: 12,
-    nom: 'Nénette la Borgne',
-    piecesOr: 5,
-  },
-  {
-    id: 13,
-    nom: 'Jacqueline la Naufragée',
-    piecesOr: 5,
-  },
-  {
-    id: 14,
-    nom: 'Edwige Jambe de bois',
-    piecesOr: 5,
-  },
-]
+    {
+      id: 2,
+      nom: 'Quartier Maître Chloé',
+      piecesOr: 6,
+    },
+    {
+      id: 3,
+      nom: 'Seconde Emma',
+      piecesOr: 6,
+    },
+    {
+      id: 4,
+      nom: 'Maître d\'équipage Fredo',
+      piecesOr: 6,
+    },
+    {
+      id: 5,
+      nom: 'Cuistot Sylvie',
+      piecesOr: 6,
+    },
+    {
+      id: 6,
+      nom: 'Maître canonnier Julien',
+      piecesOr: 6,
+    },
+    {
+      id: 7,
+      nom: 'Marie la Guerta',
+      piecesOr: 6,
+    },
+    {
+      id: 8,
+      nom: 'Navigateur Bruno',
+      piecesOr: 6,
+    },
+    {
+      id: 9,
+      nom: 'Cargueuse Cathy',
+      piecesOr: 6,
+    },
+    {
+      id: 10,
+      nom: 'Contrebandier Gauthier',
+      piecesOr: 6,
+    },
+    {
+      id: 11,
+      nom: 'Canonnière Margot',
+      piecesOr: 6,
+    },
+    {
+      id: 12,
+      nom: 'Nénette la Borgne',
+      piecesOr: 6,
+    },
+    {
+      id: 13,
+      nom: 'Jacqueline la Naufragée',
+      piecesOr: 6,
+    },
+    {
+      id: 14,
+      nom: 'Edwige Jambe de bois',
+      piecesOr: 6,
+    },
+  ]
